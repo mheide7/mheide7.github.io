b070d4d382423433e3c7353e6b5013035542b6f1
Madison Heide <mheide@nd.edu>
2019-11-04 13:14:22 -0500

    updates

--- project-two/assets/hide-reveal.js
+++ project-two/assets/hide-reveal.js
@@ -1,9 +1,16 @@
 $(document).ready(function() {
   //ID or Class name of the button goes into the first section
-  $('.grfrog').click(function() {
+  $('#reveal-button1').click(function() {
     //ID or Class name of the modal you want to appear on click goes into this second area.
-    $(".nd-content").toggleClass("invisible");
-    $(".nd-content h1").toggleClass("fighting-irish");
-    $(".nd-content h1").toggleClass("shadow");
+    $("#energy-a").removeClass("invisible");
+    $("#energy-b").addClass("invisible");
   });
-     });
+
+  $('#reveal-button2').click(function() {
+    //ID or Class name of the modal you want to appear on click goes into this second area.
+    $("#energy-a").addClass("invisible");
+    $("#energy-b").removeClass("invisible");
+  });
+
+
+});
